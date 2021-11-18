b10efe8eaca5dabf7288cdf151121c39f86f3dad
Trystan <trystandames08@gmail.com>
2021-11-18 17:53:50 +0200

    3rd Commit

--- expense-tracker-api/src/main/java/com/pairlearning/expensetracker/repositories/UserRepositoryImpl.java
+++ expense-tracker-api/src/main/java/com/pairlearning/expensetracker/repositories/UserRepositoryImpl.java
@@ -1,10 +1,11 @@
 package com.pairlearning.expensetracker.repositories;
 
-import java.beans.Statement;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.Statement;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -17,18 +18,25 @@ import com.pairlearning.expensetracker.exceptions.EtAuthException;
 @Repository
 public class UserRepositoryImpl implements UserRepository {
 	
-	private static final String SQL_CREATE = "INSERT INTO ET_USERS(USER_ID, FIRST_NAME, LAST_NAME, EMAIL,"
-			+ "PASSWORD) VALUES(NEXTVAL('ET_USERS_SEQ'), ?, ?, ?, ?)";
+	private static final String SQL_CREATE = "INSERT INTO ET_USERS(USER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD) "
+			+ "VALUES(NEXTVAL('ET_USERS_SEQ'), ?, ?, ?, ?)";
 	
 	private static final String SQL_COUNT_BY_EMAIL = "SELECT COUNT(*) FROM ET_USERS WHERE EMAIL = ?";
+	
 	private static final String SQL_FIND_BY_ID = "SELECT USER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD "
 			+ "FROM ET_USERS WHERE USER_ID = ?";
+	
+	private static final String SQL_FIND_BY_EMAIL = "SELECT USER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD "
+			+ "FROM ET_USERS WHERE EMAIL = ?";
 	
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
 	@Override
 	public Integer create(String firstName, String lastName, String email, String password) throws EtAuthException {
+		
+		String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt(10));
+		
 		try { 
 			KeyHolder keyHolder = new GeneratedKeyHolder();
 			jdbcTemplate.update(connection -> {
@@ -36,7 +44,7 @@ public class UserRepositoryImpl implements UserRepository {
 				ps.setString(1, firstName);
 				ps.setString(2, lastName);
 				ps.setString(3, email);
-				ps.setString(4, password);
+				ps.setString(4, hashedPassword);
 				return ps;
 			}, keyHolder);
 			return (Integer) keyHolder.getKeys().get("USER_ID");
@@ -47,8 +55,14 @@ public class UserRepositoryImpl implements UserRepository {
 
 	@Override
 	public User findByEmailAndPassword(String email, String password) throws EtAuthException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			User user = jdbcTemplate.queryForObject(SQL_FIND_BY_EMAIL, new Object[]{email}, userRowMapper);
+			if(!BCrypt.checkpw(password, user.getPassword()))
+				throw new EtAuthException("Invalid email/password");
+			return user;
+		}catch (EmptyResultDataAccessException e) {
+			throw new EtAuthException("Invalid email/password");
+		} 
 	}
 
 	@Override
@@ -61,7 +75,7 @@ public class UserRepositoryImpl implements UserRepository {
 		return jdbcTemplate.queryForObject(SQL_FIND_BY_ID, new Object[]{userId}, userRowMapper);
 	}
 	
-	private RowMapper<User> userRowMapper = ((rs, rowNum) -> {
+	private RowMapper<User>userRowMapper = ((rs, rowNum) -> {
 		return new User(rs.getInt("USER_ID"),
 				rs.getString("FIRST_NAME"),
 				rs.getString("LAST_NAME"),
--- expense-tracker-api/src/main/java/com/pairlearning/expensetracker/resources/UserResource.java
+++ expense-tracker-api/src/main/java/com/pairlearning/expensetracker/resources/UserResource.java
@@ -3,11 +3,21 @@ package com.pairlearning.expensetracker.resources;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pairlearning.expensetracker.Constants;
+import com.pairlearning.expensetracker.domain.User;
 import com.pairlearning.expensetracker.services.UserService;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,14 +27,37 @@ public class UserResource {
 	
 	@Autowired
 	UserService userService;
+	
+	@PostMapping("/login")
+	public ResponseEntity<Map<String, String>> loginUser(@RequestBody Map<String, Object> userMap) {
+		String email = (String) userMap.get("email");
+		String password = (String) userMap.get("password");
+		User user = userService.validateUser(email, password);
+		return new ResponseEntity<>(generateJWTTToken(user), HttpStatus.OK);
+	}
 
 	@PostMapping("/register")
-	public String registerUser(@RequestBody Map<String, Object> userMap) {
+	public ResponseEntity<Map<String, String>> registerUser(@RequestBody Map<String, Object> userMap) {
 		String firstName = (String) userMap.get("firstName");
 		String lastName = (String) userMap.get("lastName");
 		String email = (String) userMap.get("email");
 		String password = (String) userMap.get("password");
-		return firstName + ", " + lastName + ", " + email + ", " + password;
+		User user = userService.registerUser(firstName, lastName, email, password);
+		return new ResponseEntity<>(generateJWTTToken(user), HttpStatus.OK);
 	}
 	
+	private Map<String, String> generateJWTTToken(User user) {
+		long timestamp = System.currentTimeMillis();
+		String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, Constants.API_SECRET_KEY)
+				.setIssuedAt(new Date(timestamp))
+				.setExpiration(new Date(timestamp + Constants.TOKEN_VALIDITY))
+				.claim("userId", user.getUserId())
+				.claim("email", user.getEmail())
+				.claim("firstName", user.getFirstName())
+				.claim("lastName", user.getLastName())
+				.compact();
+		Map<String, String> map = new HashMap<>();
+		map.put("token", token);
+		return map;
+	}
 }
