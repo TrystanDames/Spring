ac24526cdd1f4efbf7ffc8c4555fda1749dff5c4
Trystan <trystandames08@gmail.com>
2021-12-07 11:11:14 +0200

    9th Commit

--- Spring+React/spring-react-app/src/App.js
+++ Spring+React/spring-react-app/src/App.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react';
 import './App.css';
-import FirstComponent from './learning-examples/FirstComponent';
-import SecondComponent from './learning-examples/SecondComponent';
-import ThirdComponent from './learning-examples/ThirdComponent';
+import FirstComponent from './components/learning-examples/FirstComponent';
+import SecondComponent from './components/learning-examples/SecondComponent';
+import ThirdComponent from './components/learning-examples/ThirdComponent';
+import Counter from './components/counter/Counter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Counter />
+
+      </div>
+    );
+  }
+}
+
+class LearningCompnents extends Component {
+  render() {
+    return (
+      <div className="LearningCompnents">
         My Hello World
         <FirstComponent></FirstComponent>
         <SecondComponent></SecondComponent>
