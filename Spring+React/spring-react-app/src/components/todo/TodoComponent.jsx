7a852a7fa9f425ed8aab8d43d526ec453830c144
Trystan <trystandames08@gmail.com>
2022-01-04 08:59:58 +0200

    18th Commit

--- Spring+React/spring-react-app/src/components/todo/TodoComponent.jsx
+++ Spring+React/spring-react-app/src/components/todo/TodoComponent.jsx
@@ -1,20 +1,29 @@
 import React, {Component} from 'react'
 import moment from 'moment'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
+import TodoDataService from '../../api/todo/TodoDataService'
 
 class TodoComponent extends Component {
     constructor(props) {
         super(props)
         this.state = {
             id : this.props.match.params.id,
-            description: 'Learn Form',
-            targetDate: moment(new Date()).format('YYYY-MM-DD')
+            description: '',
+            targetDate: moment(response.data.targetDate).format('YYYY-MM-DD')
         }
 
         this.onSubmit= this.onSubmit.bind(this)
         this.validate= this.validate.bind(this)
     }
 
+    componentDidMount(){
+        let username = AuthenticationService.getLoggedInUserName()
+        TodoDataService.retrieveTodo(usernmae, this.state.id)
+            .then(response => this.setState({
+                description: response.data.description,
+                targetDate: response.data.targetDate
+            }))
+    }
     validate(values) {
         let errors = {}
         if(!values.description) {
@@ -48,6 +57,7 @@ class TodoComponent extends Component {
                         validateOnChange={false}
                         validateOnBlur={false}
                         validate={this.validate}
+                        enableReinitialize={true}
                     >
                         {
                             (props) => (
--- Spring-React/src/main/java/com/rest/webservices/SpringReact/todo/TodoResource.java
+++ Spring-React/src/main/java/com/rest/webservices/SpringReact/todo/TodoResource.java
@@ -3,11 +3,14 @@ package com.rest.webservices.SpringReact.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rest.webservices.SpringReact.todo.Todo;
@@ -25,6 +28,12 @@ public class TodoResource {
 		return todoService.findAll();
 	}
 	
+	@GetMapping("/users/{username}/todos/{id}")
+	public Todo getTodo(@PathVariable String username, @PathVariable long id){
+		// Thread.sleep(3000);
+		return todoService.findById(id);
+	}
+	
 	@DeleteMapping("/users/{username}/todos/{id}")
 	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id){
 		Todo todo = todoService.deleteById(id);
@@ -32,5 +41,14 @@ public class TodoResource {
 			return ResponseEntity.noContent().build();
 		}
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PutMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(@PathVariable String username, 
+			@PathVariable long id, 
+			@RequestBody Todo todo){
+		
+		Todo todoUpdated = todoService.save(todo);
+		return new ResponseEntity<Todo>(todo, HttpStatus.OK);
 	}
 }
